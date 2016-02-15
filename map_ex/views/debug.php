1ba4fbf5b4f5a659cfe1792bf4d44cf355965ba5
Ramil Valitov <ramilvalitov@gmail.com>
2016-02-15 20:40:41 +0100

    [add] more detailed debug info

---
+++ map_ex/views/debug.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Debug module for Widgetkit 2 plugins.
+Author: Ramil Valitov
+E-mail: ramilvalitov@gmail.com
+Web: http://www.valitov.me/
+*/
+
+global $widget_id;
+global $widget_name;
+global $widget_version;
+
+$widget_name='';
+$widget_version='';
+$widget_id=$map_id;
+
+ExtractWidgetInfo();
+
+function ExtractWidgetInfo(){
+	global $widget_name;
+	global $widget_version;
+
+	$widget_name='Unknown Widget';
+	$widget_version='Unknown Widget';
+	
+	$f=@file_get_contents(__DIR__ .'/../assets/updater.js',false,null,0,1400);
+	if ($f){
+		if (preg_match_all("@.*var\s+widget_name\s*=\s*'.+';@",$f,$matches))
+			$widget_name=explode("'",$matches[0][0],3)[1];
+		if (preg_match_all("@.*var\s+widget_version\s*=\s*'.+';@",$f,$matches))
+			$widget_version=explode("'",$matches[0][0],3)[1];
+	}
+}
+
+/*
+Prints debug info string for JS console output
+$typeid defines the log warning level
+*/
+function printJSDebugString($s, $typeid=1){
+	global $widget_id;
+	global $widget_name;
+	
+	$prefix='['.$widget_name.' #'.$widget_id.'] ';	
+	$s=str_replace("'","\'",preg_replace("/\r\n|\r|\n/", ' ',$s));
+	switch($typeid){
+		case 1:
+			echo "console.info('".$prefix.$s."');";
+			break;
+		case 2:
+			echo "console.warn('".$prefix.$s."');";
+			break;
+		case 3:
+			echo "console.error('".$prefix.$s."');";
+			break;
+		default:
+			echo "console.log('".$prefix.$s."');";
+			break;
+	}
+}
+
+/*
+Prints debug info strings (array) for JS console output
+$typeid defines the log warning level
+*/
+function printJSDebugText($arrayStrings, $typeid=1){
+	foreach ($arrayStrings as $s){
+		printJSDebugString($s,$typeid);
+	}
+}
+
+function url_exists($url) {
+    if (!$fp = curl_init($url)) return false;
+    return true;
+}
--- map_ex/views/widget.php
+++ map_ex/views/widget.php
@@ -8,12 +8,24 @@ Git: https://github.com/rvalitov/widgetkit-map-ex
 */
 
 $map_id  = uniqid('wk-map-ex');
+
+require_once(__DIR__.'/debug.php');
+
+$debug_info = array();
+$debug_warning = array();
+$debug_error = array();
+
+array_push($debug_info,'Processing widget '.$widget_name.' (version '.$widget_version.')');
+array_push($debug_info,'Widget settings: '.print_r($settings,true));
+
 $markers = array();
 $width   = $settings['width']  == 'auto' ? 'auto'  : ((int)$settings['width']).'px';
 $height  = $settings['height'] == 'auto' ? '300px' : ((int)$settings['height']).'px';
 
 // Markers
+$item_id=0;
 foreach ($items as $i => $item) {
+	$item_id++;
     if (isset($item['location']) && $item['location']) {
         $marker = array(
             'lat'     => $item['location']['lat'],
@@ -35,10 +47,13 @@ foreach ($items as $i => $item) {
 			if (strlen($item['custom_pin_path'])>0){
 				$marker['pin']=trim($item['custom_pin_path']);
 				$pinoverride=true;
+				array_push($debug_info,'Unique custom pin image provided for item#'.$item_id.': '.$marker['pin']);
 			}
-			else
+			else{
 				$marker['pin']=trim($settings['custom_pin_path']);
-			
+				array_push($debug_info,'Global custom pin image will be used for item#'.$item_id.': '.$marker['pin']);
+			}
+
 			if (strlen($marker['pin'])>0){
 				//Checking for absolute URL
 				if ( (substr($marker['pin'], 0, 7) != 'http://') && (substr($marker['pin'], 0, 8) != 'https://') && (substr($marker['pin'], 0, 2) != '//') && (strlen($marker['pin'])>2) )
@@ -47,7 +62,13 @@ foreach ($items as $i => $item) {
 						$marker['pin']=JURI::base().$marker['pin'];
 					else
 						$marker['pin']=JURI::base().substr($marker['pin'], 1);
-				
+
+				array_push($debug_info,'The final URL for the custom pin of the item#'.$item_id.' is '.$marker['pin']);
+				if ($settings['debug_output'])
+					if (url_exists($marker['pin']))
+						array_push($debug_info,'The URL '.$marker['pin'].' is valid.');
+					else
+						array_push($debug_error,'Failed to check the URL '.$marker['pin']." - it doesn't exist?");
 				if ( ($pinoverride) && (is_numeric($item['custom_pin_anchor_x'])) && (is_numeric($item['custom_pin_anchor_y'])) )
 				{
 					$marker['anchor_x']=intval($item['custom_pin_anchor_x']);
@@ -60,10 +81,19 @@ foreach ($items as $i => $item) {
 						$marker['anchor_y']=intval($settings['custom_pin_anchor_y']);
 					}
 			}
+			else
+				array_push($debug_warning,'The custom image path is empty for item#'.$item_id.'. The deafult pin image will be used.');
+		}
+		else{
+			array_push($debug_info,'The configuration is set to use a default pin image for item#'.$item_id);
+			if (strlen($item['custom_pin_path'])>0)
+				array_push($debug_warning,'You have defined a custom unique pin image for item#'.$item_id.". However, this image will be ignored. To use custom images you must select 'Custom' as the 'Marker Pin Icon' option in the widget's settings. Ignore this message if the widget works as you expect.");
 		}
 
         $markers[] = $marker;
     }
+	else
+		array_push($debug_warning,'The location is missing for item#'.$item_id.'. This item will be ignored.');
 }
 
 $settings['markers'] = $markers;
@@ -81,72 +111,80 @@ if (!empty($settings['map_center'])){
     <?php echo json_encode($settings) ?>
 </script>
 
-<?php if ( ($settings['responsive']) || (!empty($settings['map_center'])) || ($settings['modal_fix']) ):?>
+<?php if ( ($settings['responsive']) || (!empty($settings['map_center'])) || ($settings['modal_fix']) || ($settings['debug_output']) ):?>
 <script>
+<?php if ($settings['debug_output']){
+	printJSDebugText($debug_info,1);
+	printJSDebugText($debug_warning,2);
+	printJSDebugText($debug_error,3);
+}?>
+<?php if ( ($settings['responsive']) || (!empty($settings['map_center'])) || ($settings['modal_fix']) ):?>
 jQuery(document).ready(function($){
 	function checkWidgetkitMaps() {
 		var item=getWidgetkitMap("<?php echo $settings['map_id']?>");
 		if (item) {
 			<?php if ($settings['responsive']):?>
-			google.maps.event.addDomListener(window, 'resize', function() {			
+			google.maps.event.addDomListener(window, 'resize', function() {
+
 				<?php if (!empty($settings['map_center'])):?>
 				item.panTo(new google.maps.LatLng(<?php echo $settings['map_center']?>));
-				<?php if ($settings['debug_output']):?>
-				console.log('[MapEx] auto pan performed to <?php echo $settings['map_center']?> for id#<?php echo $settings['map_id']?>');
+				<?php if ($settings['debug_output'])
+					printJSDebugString('Auto pan performed to '.$settings['map_center']);
+				?>
 				<?php endif;?>
-				<?php endif;?>
+
 				item.setZoom(<?php echo $settings['zoom']?>);
-				<?php if ($settings['debug_output']):?>
-				console.log('[MapEx] auto zoom performed to level <?php echo $settings['zoom']?> for id#<?php echo $settings['map_id']?>');
-				<?php endif;?>
+				<?php if ($settings['debug_output'])
+					printJSDebugString('Auto zoom performed to level '.$settings['zoom']);
+				?>
 			});
-			<?php if ($settings['debug_output']):?>
-			console.log('[MapEx] responsive setup performed for id#<?php echo $settings['map_id']?>');
-			<?php endif;?>
-			<?php endif;?>
-			
+			<?php if ($settings['debug_output'])
+				printJSDebugString('Responsive setup performed');
+			?>
+			<?php endif;//responsive?>
+
 			<?php if ( ($settings['modal_fix']) && (!empty($settings['map_center'])) ):?>
 			var modal_id='#<?php echo $settings['map_id']?>';
 			var modal_dialog=$(modal_id).closest('.uk-modal');
 			if (modal_dialog){
 				var box_id=modal_dialog.attr("id");
 				if (box_id){
-					<?php if ($settings['debug_output']):?>
-					console.log('[MapEx] modal fix setup successfull for id'+modal_id);
-					<?php endif;?>
-					$('#'+box_id).on({  
-						'show.uk.modal': function(){  
+					<?php if ($settings['debug_output'])
+						printJSDebugString('Modal fix setup successfull for modal id'.modal_id);
+					?>
+					$('#'+box_id).on({
+						'show.uk.modal': function(){
 							var map = jQuery('#<?php echo $settings['map_id']?>', '#'+box_id).first().get(0);
 							google.maps.event.trigger(map, 'resize');
 							<?php if (!empty($settings['map_center'])):?>
 							item.setCenter(new google.maps.LatLng(<?php echo $settings['map_center']?>));
 							item.panTo(new google.maps.LatLng(<?php echo $settings['map_center']?>));
-							<?php if ($settings['debug_output']):?>
-							console.log('[MapEx] map centered to <?php echo $settings['map_center']?> for id#<?php echo $settings['map_id']?>');
-							console.log('[MapEx] auto pan performed to <?php echo $settings['map_center']?> for id#<?php echo $settings['map_id']?>');
-							<?php endif;?>
-							<?php endif;?>
-							
+							<?php if ($settings['debug_output']){
+								printJSDebugString('Map centered to '.$settings['map_center']);
+								printJSDebugString('Auto pan performed to '.$settings['map_center']);
+							}?>
+							<?php endif;//map_center?>
+
 							item.setZoom(<?php echo $settings['zoom']?>);
-							<?php if ($settings['debug_output']):?>
-							console.log('[MapEx] auto zoom performed to level <?php echo $settings['zoom']?> for id#<?php echo $settings['map_id']?>');
-							<?php endif;?>
-						
-							<?php if ($settings['debug_output']):?>
-							console.log('[MapEx] modal fix performed for id'+modal_id);
-							<?php endif;?>
+							<?php if ($settings['debug_output'])
+								printJSDebugString('Auto zoom performed to level '.$settings['zoom']);
+							?>
+
+							<?php if ($settings['debug_output'])
+								printJSDebugString('Modal fix performed for modal id'.modal_id);
+							?>
 						}
 					});
 				}
 				<?php if ($settings['debug_output']):?>
 				else
-					console.log('[MapEx] failed to get id for modal dailog for id'+modal_id);
+					<?php printJSDebugString('Failed to find modal with id'.modal_id,3);?>
 				<?php endif;?>
-				
+
 			}
 			<?php if ($settings['debug_output']):?>
 			else
-				console.log('[MapEx] failed to find modal dailog for id'+modal_id);
+				<?php printJSDebugString('Failed to find modal with id'.modal_id,3);?>
 			<?php endif;?>
 
 			<?php endif;//modal fix?>
@@ -156,5 +194,6 @@ jQuery(document).ready(function($){
 	}
 	setTimeout(checkWidgetkitMaps,1000);
 });
+<?php endif;?>
 </script>
 <?php endif;?>
