a67ce6c8aa0976f949710c52dd99aed9d38f2675
Ramil Valitov <ramilvalitov@gmail.com>
2018-02-20 14:30:23 +0100

    updated the link to styling guide, closes #70

--- map_ex/views/edit.php
+++ map_ex/views/edit.php
@@ -410,7 +410,7 @@ $is_api_key_supported=$plugin->isWKAPIKeySupported($app);
 				</div>
 				
 				<div class="uk-form-row uk-text-center" ng-if="widget.data['styling_mode'] == 'json'">
-					<a href="http://googlemaps.github.io/js-samples/styledmaps/wizard/index.html" class="uk-button uk-button-primary uk-container-center" target="_blank">{{'Launch the Styled Maps Wizard'|trans}}<i class="uk-icon uk-icon-external-link uk-margin-small-left"></i></a>
+					<a href="https://mapstyle.withgoogle.com/" class="uk-button uk-button-primary uk-container-center" target="_blank">{{'Launch the Styled Maps Wizard'|trans}}<i class="uk-icon uk-icon-external-link uk-margin-small-left"></i></a>
 				</div>
 				
                 <div class="uk-form-row" ng-if="widget.data['styling_mode'] == ''">
