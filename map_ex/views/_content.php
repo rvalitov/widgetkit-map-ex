fb1868311d367fd18af3002a5c497b6e485105f2
Ramil Valitov <ramilvalitov@gmail.com>
2021-03-02 01:37:10 +0300

    [fix] show correct content

--- map_ex/views/_content.php
+++ map_ex/views/_content.php
@@ -8,7 +8,7 @@ Git: https://github.com/rvalitov/widgetkit-map-ex
 */
 
 // Media Width
-$media_width = '{wk}-width-' . $settings['media_breakpoint'] . '-' . $settings['media_width'];
+$media_width = '{wk}-width-' . $settings['media_width'] . '@' . $settings['media_breakpoint'];
 
 switch ($settings['media_width']) {
     case '1-5':
@@ -31,27 +31,30 @@ switch ($settings['media_width']) {
         break;
 }
 
-$content_width = '{wk}-width-' . $settings['media_breakpoint'] . '-' . $content_width;
+$content_width = '{wk}-width-' . $content_width . '@' . $settings['media_breakpoint'];
 
 // Title Size
 switch ($settings['title_size']) {
+    case 'medium':
+        $title_size = '{wk}-heading-medium {wk}-margin-remove-top';
+        break;
     case 'large':
-        $title_size = '{wk}-heading-large {wk}-margin-top-remove';
+        $title_size = '{wk}-heading-large {wk}-margin-remove-top';
         break;
     default:
-        $title_size = '{wk}-' . $settings['title_size'] . ' {wk}-margin-top-remove';
+        $title_size = '{wk}-' . $settings['title_size'] . ' {wk}-margin-remove-top';
 }
 
 // Link Style
 switch ($settings['link_style']) {
     case 'button':
-        $link_style = '{wk}-button';
+        $link_style = '{wk}-button {wk}-button-default';
         break;
     case 'primary':
         $link_style = '{wk}-button {wk}-button-primary';
         break;
     case 'button-large':
-        $link_style = '{wk}-button {wk}-button-large';
+        $link_style = '{wk}-button {wk}-button-large {wk}-button-default';
         break;
     case 'primary-large':
         $link_style = '{wk}-button {wk}-button-large {wk}-button-primary';
@@ -72,10 +75,9 @@ $link_target = ($settings['link_target']) ? ' target="_blank"' : '';
 // Social Buttons
 $socials = '';
 if ($settings['social_buttons']) {
-    $socials .= $item['twitter'] ? '<div><a class="{wk}-icon-button {wk}-icon-twitter" href="'. $item->escape('twitter') .'"></a></div>': '';
-    $socials .= $item['facebook'] ? '<div><a class="{wk}-icon-button {wk}-icon-facebook" href="'. $item->escape('facebook') .'"></a></div>': '';
-    $socials .= $item['google-plus'] ? '<div><a class="{wk}-icon-button {wk}-icon-google-plus" href="'. $item->escape('google-plus') .'"></a></div>': '';
-    $socials .= $item['email'] ? '<div><a class="{wk}-icon-button {wk}-icon-envelope-o" href="mailto:'. $item->escape('email') .'"></a></div>': '';
+    $socials .= $item['twitter'] ? '<div><a class="{wk}-icon-button" {wk}-icon="twitter" href="'. $item->escape('twitter') .'"></a></div>': '';
+    $socials .= $item['facebook'] ? '<div><a class="{wk}-icon-button" {wk}-icon="facebook" href="'. $item->escape('facebook') .'"></a></div>': '';
+    $socials .= $item['email'] ? '<div><a class="{wk}-icon-button" {wk}-icon="mail" href="mailto:'. $item->escape('email') .'"></a></div>': '';
 }
 
 // Second Image as Overlay
@@ -98,7 +100,7 @@ if ($item->type('media') == 'image') {
     $attrs['alt'] = strip_tags($item['title']);
 
     $attrs['class'] .= '{wk}-responsive-width ' . $border;
-    $attrs['class'] .= ($settings['media_animation'] != 'none'  && !$media2) ? ' {wk}-overlay-' . $settings['media_animation'] : '';
+    $attrs['class'] .= ($settings['media_animation'] != 'none'  && !$media2) ? ' {wk}-transition-' . $settings['media_animation']. ' {wk}-transition-opaque' : '';
 
     $width  = ($settings['image_width'] != 'auto') ? $settings['image_width'] : '';
     $height = ($settings['image_height'] != 'auto') ? $settings['image_height'] : '';
@@ -110,7 +112,7 @@ if ($item->type('media') == 'video') {
 }
 
 if ($item->type('media') == 'iframe') {
-    $attrs['class'] = '{wk}-responsive-width';
+    $attrs['{wk}-responsive'] = true;
 }
 
 $attrs['width']  = ($width) ? $width : '';
@@ -125,8 +127,8 @@ if (($item->type('media') == 'image') && ($settings['image_width'] != 'auto' || 
 // Second Image as Overlay
 if ($media2) {
 
-    $attrs['class'] .= ' {wk}-overlay-panel {wk}-overlay-image';
-    $attrs['class'] .= ($settings['media_animation'] != 'none') ? ' {wk}-overlay-' . $settings['media_animation'] : '';
+    $attrs['class'] .= ' {wk}-position-cover';
+    $attrs['class'] .= ($settings['media_animation'] != 'none') ? ' {wk}-transition-' . $settings['media_animation'] : '{wk}-transition-fade';
 
     $media2 = $item->thumbnail($media2, $width, $height, $attrs);
 }
@@ -134,14 +136,16 @@ if ($media2) {
 // Link and Overlay
 if ($item['link'] && ($settings['media_overlay'] == 'link' || $settings['media_overlay'] == 'icon' || $settings['media_overlay'] == 'image')) {
 
-    $media = '<div class="{wk}-overlay {wk}-overlay-hover ' . $border . '">' . $media;
+    $media = '<div class="{wk}-inline-clip {wk}-transition-toggle ' . $border . '">' . $media;
 
     if ($media2) {
         $media .= $media2;
     }
 
     if ($settings['media_overlay'] == 'icon') {
-        $media .= '<div class="{wk}-overlay-panel {wk}-overlay-background {wk}-overlay-icon {wk}-overlay-' . $settings['overlay_animation'] . '"></div>';
+        $media .= '<div class="{wk}-overlay-primary {wk}-position-cover {wk}-transition-fade">';
+        $media .= '<div class="{wk}-position-center"><span class="' . ($settings['overlay_animation'] != 'fade' ? '{wk}-transition-opaque {wk}-transition-' . $settings['overlay_animation'] : '') . '" {wk}-overlay-icon></span></div>';
+        $media .= '</div>';
     }
 
     $media .= '<a class="{wk}-position-cover" href="' . $item->escape('link') . '"' . $link_target . '></a>';
@@ -149,43 +153,43 @@ if ($item['link'] && ($settings['media_overlay'] == 'link' || $settings['media_o
 }
 
 if ($socials && $settings['media_overlay'] == 'social-buttons') {
-    $media  = '<div class="{wk}-overlay {wk}-overlay-hover ' . $border . '">' . $media;
-    $media .= '<div class="{wk}-overlay-panel {wk}-overlay-background {wk}-overlay-' . $settings['overlay_animation'] . ' {wk}-flex {wk}-flex-center {wk}-flex-middle {wk}-text-center"><div>';
-    $media .= '<div class="{wk}-grid {wk}-grid-small" data-{wk}-grid-margin>' . $socials . '</div>';
+    $media  = '<div class="{wk}-inline-clip {wk}-transition-toggle' . $border . '">' . $media;
+    $media .= '<div class="{wk}-overlay {wk}-overlay-primary {wk}-position-cover {wk}-transition-' . $settings['overlay_animation'] . ' {wk}-flex {wk}-flex-center {wk}-flex-middle {wk}-text-center"><div>';
+    $media .= '<div class="{wk}-grid {wk}-grid-small" {wk}-grid>' . $socials . '</div>';
     $media .= '</div></div>';
     $media .= '</div>';
 }
 ?>
 
-<div class="{wk}-text-<?php echo $settings['text_align']; ?>">
+<div class="{wk}-text-<?= $settings['text_align'] ?>">
 
     <?php if ($item['media'] && $settings['media'] && $settings['media_align'] == 'top') : ?>
-    <div class="{wk}-margin {wk}-text-center"><?php echo $media; ?></div>
+    <div class="{wk}-margin {wk}-text-center"><?= $media ?></div>
     <?php endif; ?>
 
     <?php if ($item['media'] && $settings['media'] && in_array($settings['media_align'], array('left', 'right'))) : ?>
-    <div class="{wk}-grid" data-{wk}-grid-margin>
-        <div class="<?php echo $media_width ?><?php if ($settings['media_align'] == 'right') echo ' {wk}-float-right {wk}-flex-order-last-' . $settings['media_breakpoint'] ?>">
-            <?php echo $media; ?>
+    <div class="{wk}-grid" {wk}-grid>
+        <div class="<?= $media_width ?><?php if ($settings['media_align'] == 'right') echo '  {wk}-flex-last@' . $settings['media_breakpoint'] ?>">
+            <?= $media ?>
         </div>
-        <div class="<?php echo $content_width ?>">
+        <div class="<?= $content_width ?>">
             <div class="{wk}-panel">
     <?php endif; ?>
 
     <?php if ($item['title'] && $settings['title']) : ?>
-    <h3 class="<?php echo $title_size; ?>"><?php echo $item['title']; ?></h3>
+    <<?= $settings['title_element'] ?> class="<?= $title_size ?>"><?= $item['title'] ?></<?= $settings['title_element'] ?>>
     <?php endif; ?>
 
     <?php if ($item['media'] && $settings['media'] && $settings['media_align'] == 'bottom') : ?>
-    <div class="{wk}-margin {wk}-text-center"><?php echo $media; ?></div>
+    <div class="{wk}-margin {wk}-text-center"><?= $media ?></div>
     <?php endif; ?>
 
     <?php if ($item['content'] && $settings['content']) : ?>
-    <div class="{wk}-margin"><?php echo $item['content']; ?></div>
+    <div class="{wk}-margin"><?= $item['content'] ?></div>
     <?php endif; ?>
 
     <?php if ($socials && ($settings['media_overlay'] != 'social-buttons')) : ?>
-    <div class="{wk}-grid {wk}-grid-small {wk}-flex-<?php echo $settings['text_align']; ?>" data-{wk}-grid-margin><?php echo $socials; ?></div>
+    <div class="{wk}-grid {wk}-grid-small {wk}-flex-<?= $settings['text_align'] ?>" {wk}-grid><?= $socials ?></div>
     <?php endif; ?>
 
     <?php if ($item['link'] && $settings['link']) : ?>
